e25542809084113515306830aede488b03e2a159
Mitsu <sdkthiam4@gmail.com>
2026-02-04 20:18:41 +0100

    Add of parse_line logic

--- log_analyzer.py
+++ log_analyzer.py
@@ -8,3 +8,19 @@ class LogEntry:
     level: str
     message: str
 
+def parse_line(line: str) -> LogEntry | None:
+    line = line.strip()
+
+    if not line:
+        return None
+    try:
+        timestamp_str = line[:19]
+        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+
+        rest = line[20:]
+        level, message = rest.split(" ", 1)
+
+        return LogEntry(timestamp=timestamp, level=level, message=message)
+
+    except(ValueError, IndexError):
+        return None
